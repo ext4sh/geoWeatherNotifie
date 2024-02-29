866798427b0646cb99f9c5726c0fc0e7784a3303
ext4sh <138382892+ext4sh@users.noreply.github.com>
2024-03-01 00:20:38 +0200

    Create live_weather.py

---
+++ live_weather.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import re
+
+from bs4 import BeautifulSoup
+from notifypy import Notify
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+geo_request = requests.get("https://geolocation-db.com/jsonp")
+geo_data = re.findall(r"\((.*?)\)", geo_request.text)[0]
+geo_json = json.loads(geo_data)
+
+lat, lon = geo_json["latitude"], geo_json["longitude"]
+url = f"https://weather.com/en-IN/weather/today/l/{lat},{lon}"
+
+htmldata = getdata(url) 
+soup = BeautifulSoup(htmldata, 'html.parser')
+print(soup.prettify())
+
+current_temperature = soup.find("span",
+                                class_="CurrentConditions--tempValue--MHmYY")
+
+if current_temperature:
+    temp = current_temperature.text
+else:
+    temp = "Error"
+
+result = f"Current temperature {temp} in {geo_json['city']}"
+
+notification = Notify()
+notification.title = "Weather"
+notification.message = result
+notification.send()
